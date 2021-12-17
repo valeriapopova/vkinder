891459e7536bcc55dd861d78018b6911c0d838fc
valeriapopova <iamvaleriapopova@yandex.ru>
2021-12-17 17:47:43 +0400

    3

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 import sqlalchemy as sq
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 from config import *
-import psycopg2
 
 
 Base = declarative_base()
@@ -17,6 +16,10 @@ class User(Base):
     first_name = sq.Column(sq.String)
     last_name = sq.Column(sq.String)
     id_vk = sq.Column(sq.Integer)
+    city = sq.Column(sq.Integer)
+    age_from = sq.Column(sq.Integer)
+    age_to = sq.Column(sq.Integer)
+    sex = sq.Column(sq.Integer)
 
 
 class Match(Base):
@@ -25,17 +28,8 @@ class Match(Base):
     first_name = sq.Column(sq.String)
     last_name = sq.Column(sq.String)
     link = sq.Column(sq.String)
-    city = sq.Column(sq.String)
     id_vk = sq.Column(sq.Integer, unique=True)
     id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id', ondelete='CASCADE'))
-
-
-class Photos(Base):
-    __tablename__ = 'photos'
-    id = sq.Column(sq.Integer, primary_key=True)
-    photo_link = sq.Column(sq.String)
-    likes_count = sq.String(sq.Integer)
-    id_match = sq.Column(sq.Integer, sq.ForeignKey('match.id', ondelete='CASCADE'))
 
 
 class BlackList(Base):
@@ -45,8 +39,6 @@ class BlackList(Base):
     first_name = sq.Column(sq.String)
     last_name = sq.Column(sq.String)
     link = sq.Column(sq.String)
-    photo_link = sq.Column(sq.String)
-    city = sq.Column(sq.String)
     user_id = sq.Column(sq.Integer, sq.ForeignKey('user.id', ondelete='CASCADE'))
 
 
@@ -56,51 +48,37 @@ def create_tables():
 
 def add_user(id_vk):
     session.expire_on_commit = False
-    add_new_user = User(id_vk=id_vk)
-    session.add(add_new_user)
-    session.commit()
-    return True
-
-
-def add_match(first_name, last_name, city, link , id_user, id_vk):
-    session.expire_on_commit = False
-    add_new_match = Match(id_vk=id_vk, first_name=first_name, lats_name=last_name, city=city, link=link, id_user=id_user)
-    session.add(add_new_match)
-    session.commit()
-    return True
-
-
-def add_photo(photo_link, likes_count, id_match):
-    session.expire_on_commit = False
-    photo = Photos(photo_link=photo_link, likes_count=likes_count, id_match=id_match)
-    session.add(photo)
-    session.commit()
-    return True
-
-
-def add_to_black_list(id_vk, first_name , last_name, link, photo_link, city, user_id):
-    add_bl = BlackList(id_vk=id_vk, first_name=first_name, last_name=last_name, link=link,
-                       photo_link=photo_link, city=city, user_id=user_id)
-    session.add(add_bl)
-    session.commit()
-    return True
-
-
-def user_registration(id_vk):
     new_user = User(id_vk=id_vk)
     session.add(new_user)
     session.commit()
     return True
 
 
-def delete_match(vk_id):
-    user_match = session.query(Match).filter_by(id_vk=vk_id).first()
-    session.delete(user_match)
+def add_match(first_name, last_name, link, id_vk):
+    session.expire_on_commit = False
+    add_new_match = Match(first_name=first_name, last_name=last_name, link=link, id_vk=id_vk)
+    session.add(add_new_match)
     session.commit()
+    return True
 
 
-def delete_from_bl(vk_id):
-    user_bl = session.query(BlackList).filter_by(id_vk=vk_id).first()
-    session.delete(user_bl)
+def register_user(id_vk):
+    new_user = User(id_vk=id_vk)
+    session.add(new_user)
     session.commit()
+    return True
+
+
+def check(user_id):
+    current_user_id = session.query(User).filter_by(vk_id=user_id).first()
+    fav_users = session.query(Match).fitler_by(id_user=current_user_id.id).all()
+    return fav_users
+
+
+def add_to_black_list(id_vk, first_name, last_name, link):
+    add_bl = BlackList(id_vk=id_vk, first_name=first_name, last_name=last_name, link=link)
+    session.add(add_bl)
+    session.commit()
+    return True
+
 
--- main.py
+++ main.py
@@ -1,41 +1,250 @@
 from random import randrange
+from sqlite3 import IntegrityError
+
+import db
 from config import *
 import requests
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from functions_vk import search_partners, getting_photos
 
 vk = vk_api.VkApi(token=token)
 user_api = vk.get_api()
 
 
 def write_msg(user_id, message, attachment=''):
-    vk.method('messages.send',
-              {'user_id': user_id,
-               'message': message,
-               'random_id': randrange(10 ** 7),
-                'attachment': attachment})
+    vk_group.method('messages.send',
+                    {'user_id': user_id,
+                     'message': message,
+                     'random_id': randrange(10 ** 7),
+                     'attachment': attachment})
 
 
-vk = vk_api.VkApi(token=group_token)
-api = vk.get_api()
-longpoll = VkLongPoll(vk, 209285110)
+vk_group = vk_api.VkApi(token=group_token)
+api = vk_group.get_api()
+longpoll = VkLongPoll(vk_group, 209285110)
 
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            request = event.text
-            if request == "привет" or "Привет":
-                write_msg(event.user_id, f"Хей, {event.user_id}, хочешь найти парнера/партнёрку?\n "
-                                         f"Чтобы начать поиск,отправь СТАРТ")
-            elif request == "пока" or "Пока":
-                write_msg(event.user_id, "До свидания")
+class Bot:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.user = db.User
+        self.first_name = ''
+        self.last_name = ''
+        self.city = 0
+        self.age_from = 0
+        self.age_to = 0
+        self.sex = ''
+        self.offset = 0
+        self.match_id = 0
+        self.top_photos = ''
+        self.match_name = ''
+        self.match_lastname = ''
+
+    def get_info(self):
+        response = requests.get(
+            'https://api.vk.com/method/users.get',
+            {'user_ids': self.user_id, 'access_token': token, 'v': version})
+        for user_info in response.json()['response']:
+            self.first_name = user_info['first_name']
+            self.last_name = user_info['last_name']
+        return self.first_name + ' ' + self.last_name
+
+    def get_city(self, city):
+        result = vk.method('database.getCities', {'country_id': 1, 'count': 1, 'q': city})
+        if not result['items']:
+            self.city = 0
+            return self.city
+        else:
+            for city_ in result['items']:
+                self.city = city_['id']
+                return self.city
+
+    def get_age_from(self):
+        write_msg(self.user_id, 'Введите минимальный возраст будущего партнера')
+        for age_event in longpoll.listen():
+            if age_event.type == VkEventType.MESSAGE_NEW:
+                if age_event.to_me:
+                    try:
+                        if int(age_event.text) >= 14:
+                            self.age_from = age_event.text
+                            return self.age_from
+                        else:
+                            write_msg(self.user_id, 'Минимальный возраст 14 лет!Повторите ввод')
+                    except:
+                        write_msg(self.user_id, 'Минимальный возраст 14 лет!')
+
+    def get_age_to(self):
+        write_msg(self.user_id, 'Введите максимальный возраст будущего партнера')
+        for age_event in longpoll.listen():
+            if age_event.type == VkEventType.MESSAGE_NEW:
+                if age_event.to_me:
+                    try:
+                        if int(age_event.text) < 110:
+                            self.age_to = age_event.text
+                            return self.age_to
+                        else:
+                            write_msg(self.user_id, 'Максимальный возраст 110 лет,побойтесь Бога!Повторите ввод')
+                    except:
+                        write_msg(self.user_id, 'Максимальный возраст 110 лет,побойтесь Бога!Повторите ввод')
+
+    def gender(self):
+        write_msg(self.user_id, 'Введите пол партнера(1-если женский,2-если мужской,3-если любой)')
+        for gender_event in longpoll.listen():
+            if gender_event.type == VkEventType.MESSAGE_NEW:
+                if gender_event.to_me:
+                    res = gender_event.text
+                    if res == "1" or res == "2" or res == "3":
+                        self.sex = gender_event.message
+                        return self.sex
+                    else:
+                        self.sex = 0
+                        return self.sex
+
+    def registration(self):
+        write_msg(self.user_id, "Вы прошли регистрацию,"
+                                "чтобы начать поиск ,напишите СТАРТ ")
+        db.register_user(self.user_id)
+
+    def search_partners(self, age_from, age_to, sex, city):
+        result = vk.method('users.search', {
+            'age_from': age_from,
+            'age_to': age_to,
+            'sex': sex,
+            'city': city,
+            'status': 6,
+            'has_photo': 1
+        })
+        for items in result['items']:
+            is_closed = items['is_closed']
+            if is_closed:
+                self.offset += 1
             else:
-                write_msg(event.user_id, "Не понял вашего ответа...")
+                self.match_id = items["id"]
+                self.match_name = items['first_name']
+                self.match_lastname = items['last_name']
+
+    def getting_photos(self, user_id):
+        result = requests.get(
+            'https://api.vk.com/method/photos.get', {
+                'owner_id': user_id,
+                'album_id': 'profile',
+                'access_token': token,
+                'v': version,
+                'extended': 1,
+                'photos_sizes': 1
+            })
+        all_photos = []
+        sorted_res = sorted(result.json()['response']['items'], key=lambda x: x['likes']['count'], reverse=True)
+        for id_photo in sorted_res:
+            all_photos.append(f"photo{self.match_id}_{id_photo['id']}")
+        self.top_photos = ','.join(all_photos[:3])
+        return self.top_photos
+
+    def search_pair(self):
+        self.get_info()
+        db.create_tables()
+        db.register_user(self.user_id)
+        write_msg(self.user_id, f"В каком городе будем искать партнера?")
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                if event.to_me:
+                    if self.get_city(event.message) == 0:
+                        write_msg(self.user_id, f"Не знаю такого города")
+                    else:
+                        self.gender()
+                        self.get_age_from()
+                        self.get_age_to()
+                        if self.age_from > self.age_to:
+                            write_msg(self.user_id, f"Неверный интервал возраста")
+                            self.search_pair()
+                        self.user = db.User(first_name=self.first_name, last_name=self.last_name, city=self.city,
+                                            id_vk=self.user_id, age_from=self.age_from, age_to=self.age_to,
+                                            sex=self.sex)
+                        db.add_user(self.user_id)
+                        self.search_partners(self.age_from, self.age_to, self.sex, self.city)
+                        self.getting_photos(self.match_id)
+                        profile_link = f'https://vk.com/id{self.match_id}'
+                        write_msg(self.user_id, f'Имя: {self.match_name}\n'
+                                                f'Фамилия: {self.match_lastname}\nСсылка: {profile_link}',
+                                  self.top_photos)
+                        return self.search_pair_()
+
+    def search_pair_(self):
+        write_msg(self.user_id, f"Как Вам? Хотите добавить в избранное?"
+                                f"Напишите ДА ,чтобы добавить в избранное, ЧС - чтобы добавить в черный список, "
+                                f"что угодно ,чтобы продолжить")
+        while True:
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    if event.to_me:
+                        profile_link = f'https://vk.com/id{self.match_id}'
+                        if event.message == 'Да' or event.message == 'да' or event.message == 'ДА':
+                            db.add_user(self.user_id)
+                            try:
+                                db.add_match(id_vk=self.match_id, first_name=self.match_name,
+                                             last_name=self.match_lastname,
+                                             link=profile_link)
+                                write_msg(self.user_id, 'Пользователь добавлен в избранное,ищем дальше(да/нет)?')
+                            except IntegrityError:
+                                write_msg(self.user_id, 'Пользователь уже в избранном,ищем дальше(да/нет),'
+                                                        'чтобы посмотреть список избранных напишите +?')
+                        elif event.message == '+':
+                            db.check(self.user_id)
+                        elif event.message == 'ДАЛЕЕ' or event.message == 'далее' or event.message == 'Далее' or \
+                                event.message == 'Да' or event.message == 'да' or event.message == 'ДА':
+                            write_msg(self.user_id, 'Ищем дальше')
+                            self.offset += 1
+                            self.search_partners(self.age_from, self.age_to, self.sex, self.city)
+                            self.getting_photos(self.match_id)
+                            write_msg(self.user_id, f'Имя: {self.match_name}\n'
+                                                    f'Фамилия: {self.match_lastname}\nСсылка: {profile_link}',
+                                      self.top_photos)
+                            return self.search_pair_()
+                        elif event.message == 'нет' or event.message == 'НЕТ' or event.message == 'Нет':
+                            write_msg(self.user_id, 'Чтобы продолжить поиск ,напишите ДАЛЕЕ,'
+                                                    'чтобы начать поиск с новыми параметрами СТАРТ'
+                                                    'чтобы закончить СТОП')
+                        elif event.message == 'стоп' or event.message == 'СТОП' or event.message == 'Стоп':
+                            pass
+
+                        elif request == "СТАРТ" or request == "старт" or request == "Старт":
+                            bot.search_pair()
+
+                        elif event.message == 'Чс' or event.message == 'ЧС' or event.message == 'чс':
+                            db.add_to_black_list(id_vk=self.match_id, first_name=self.match_name,
+                                                 last_name=self.match_lastname,
+                                                 link=profile_link)
+                            write_msg(self.user_id, 'Пользователь добавлен в черный список,ищем дальше(да/нет)?')
+
+                        else:
+                            write_msg(self.user_id, 'Ищем дальше')
+                            self.offset += 1
+                            self.search_partners(self.age_from, self.age_to, self.sex, self.city)
+                            self.getting_photos(self.match_id)
+                            write_msg(self.user_id, f'Имя: {self.match_name}\n'
+                                                    f'Фамилия: {self.match_lastname}\nСсылка: {profile_link}',
+                                      self.top_photos)
+                            return self.search_pair_()
 
 
-
-
-
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    for new_event in longpoll.listen():
+        if new_event.type == VkEventType.MESSAGE_NEW:
+            if new_event.to_me:
+                bot = Bot(new_event.user_id)
+                request = new_event.text
+                if request == "привет" or request == "Привет":
+                    bot.get_info()
+                    write_msg(new_event.user_id, f"Хей, {bot.first_name}, "
+                                                 f"хочешь найти партнера/партнёрку? Для начала нужно пройти "
+                                                 f"регистрацию, напиши РЕГИСТРАЦИЯ ")
+                elif request == "регистрация" or request == "РЕГИСТРАЦИЯ" or request == "Регистрация":
+                    bot.registration()
+                elif request == "СТАРТ" or request == "старт" or request == "Старт":
+                    bot.search_pair()
+                elif request == "Стоп" or request == "стоп" or request == "СТОП":
+                    pass
+                elif request == "пока" or request == "Пока":
+                    write_msg(new_event.user_id, "До свидания")
+                else:
+                    write_msg(new_event.user_id, "Не понял вашего ответа...Напишите ПРИВЕТ, чтобы начать")
